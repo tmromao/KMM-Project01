24de1e335ea87ddc9e6b116b1aef6ec8144b9df8
tromao <tiagomromao@gmail.com>
2023-06-15 08:45:19 +0100

    Chapt47-Proibir screenshots, impedir printscreen da tela jetpack compose.

--- androidApp/src/main/java/com/example/kmmproject01/android/ui/activity/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmmproject01/android/ui/activity/MainActivity.kt
@@ -1,28 +1,69 @@
 package com.example.kmmproject01.android.ui.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.kmmproject01.android.MyApplicationTheme
+import com.example.kmmproject01.android.ui.theme.AndroidAppTheme
 import com.example.kmmproject01.di.DI
+import com.example.kmmproject01.resources.Resources
 import com.example.kmmproject01.utils.AndroidMainApp
 import com.example.kmmproject01.viewmodels.EntryViewModel
+import com.example.kmmproject01.viewmodels.MainActivityViewModel
 
 class MainActivity : ComponentActivity() {
+
+    // ViewModel para enderecar auditoria de seguranca
+    private val model by viewModels<MainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Endereca auditoria de seguranca: Hide recent thumbnails
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            // Endereca auditoria de seguranca: Hide Multi-Windows Mode
+            model.toggleHideThumbnailState(
+                hide = isInMultiWindowMode || isInPictureInPictureMode
+            )
+        }
 
         val entryViewModel = EntryViewModel()
         Log.d("TESTANDO", DI.Native.environment.name)
 
         setContent {
-            MyApplicationTheme {
-                // KtorVid(viewModel = entryViewModel)
-                //CustomDialogVideo()
-                ModalBottomSheetVideo()
+
+            // Escutar, observar o estado de "hideThumbnailState" e atualizar a UI
+            val hideThumbnail by model.hideThumbnail.collectAsState()
+
+            AndroidAppTheme {
+                if (hideThumbnail) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Resources.Theme.background.getColor())
+                    )
+                } else{
+
+                    // KtorVid(viewModel = entryViewModel)
+                    //CustomDialogVideo()
+                    ModalBottomSheetVideo()
+                }
+
+
             }
         }
     }
---
+++ shared/src/commonMain/kotlin/com/example/kmmproject01/viewmodels/MainActivityViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.kmmproject01.viewmodels
+
+import com.example.kmmproject01.BaseSharedViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+// 1) COMO ENDERECAR ISSUE DE SEGURANçA APOS AUDITORIA
+// 2) COMO PREVINIR QUE USUÁRO FACA PRINTSCREEN DO SEU APP (SEGURO/BANCARIO)
+// 3) COMO EVITAR WORKAROUND ATRAVES DO MULTIWINDOW MODE
+
+class MainActivityViewModel : BaseSharedViewModel() {
+
+    // Endereça auditoria de segurança:
+    // hide recent thumbnails plus multi-screen mode
+    private val _hideThumbnail = MutableStateFlow(false)
+    val hideThumbnail: StateFlow<Boolean>
+        get() = _hideThumbnail
+
+    fun toggleHideThumbnailState(hide: Boolean) {
+        scope.launch {
+            _hideThumbnail.emit(hide)
+        }
+    }
+}
